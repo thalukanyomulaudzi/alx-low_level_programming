9d079d59bcdf81f82d990b7e4bb6ddede86d43e6
Thalukanyo Mulaudzi <thalu.tuks@gmail.com>
2022-09-08 19:20:55 +0000

    task 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle\n";
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Mystr);
 
