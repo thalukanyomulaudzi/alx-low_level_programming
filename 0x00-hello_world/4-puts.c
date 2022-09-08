efc1eea050b27bab5c21b45e8f7ca263dc0276bc
Thalukanyo Mulaudzi <thalu.tuks@gmail.com>
2022-09-08 19:07:28 +0000

    task 3

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	//string initialisation
+	
+	char Mystr[] = "Programming is like building a multilingual puzzle\n";
+	puts(Mystr);
+	
+	return (0);
+}
