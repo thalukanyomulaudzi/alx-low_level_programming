e6c649b535b2b046b3a6d2dc074f64b2140898c7
Thalukanyo Mulaudzi <thalu.tuks@gmail.com>
2022-09-09 19:32:57 +0000

    task 3

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if(c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		
+		c++;
+		
+	}
+	putchar('\n');
+
+	return (0);
+}
