2ecb831e2d03521a1ba90fbfef5f93c17b0bdcb3
Thalukanyo Mulaudzi <thalu.tuks@gmail.com>
2022-09-11 05:53:17 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+		}
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
