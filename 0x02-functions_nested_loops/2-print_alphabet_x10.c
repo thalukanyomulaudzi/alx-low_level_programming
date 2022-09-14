0c366658e1add700b2cd07eac2586be6b090fe1b
Thalukanyo Mulaudzi <thalu.tuks@gmail.com>
2022-09-14 20:06:47 +0000

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
