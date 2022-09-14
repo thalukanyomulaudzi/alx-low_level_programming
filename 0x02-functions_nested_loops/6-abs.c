172e5d05e3844dc3a82e108482ee576a145cd53e
Thalukanyo Mulaudzi <thalu.tuks@gmail.com>
2022-09-14 20:39:28 +0000

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * @c: arguement
+ * Return: 0
+ */
+int _abs(int c)
+{
+	if (c > 0 || c == 0)
+	{
+		return (c);
+	}
+	else
+	{
+		return (c * -1);
+	}
+}
