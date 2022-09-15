f2ce5662c2f863a56ea1d72940490a814b2b8576
Thalukanyo Mulaudzi <thalu.tuks@gmail.com>
2022-09-15 02:30:20 +0000

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_to_98 - Entry point
+ *@n: counting number
+ */
+void print_to_98(int n)
+{
+	if (n > 97)
+	{
+		while (n > 98)
+		{
+			printf("%d,", n--);
+		}
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d,", n++);
+		}
+		printf("%d\n", n);
+	}
+}
